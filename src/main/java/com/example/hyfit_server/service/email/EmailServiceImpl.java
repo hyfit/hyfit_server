4130610fc80d968591baaf005920f74630563b21
DevYoung00 <dkdud203@naver.com>
2023-03-28 11:25:12 +0900

    Fix: email 인증 6자리 숫자로 변경

--- src/main/java/com/example/hyfit_server/service/email/EmailServiceImpl.java
+++ src/main/java/com/example/hyfit_server/service/email/EmailServiceImpl.java
@@ -43,28 +43,39 @@ public class EmailServiceImpl implements EmailService{
         return message;
     }
 
+//    public static String createKey() {
+//        StringBuffer key = new StringBuffer();
+//        Random rnd = new Random();
+//
+//        for (int i = 0; i < 8; i++) { // 인증코드 8자리
+//            int index = rnd.nextInt(3); // 0~2 까지 랜덤
+//
+//            switch (index) {
+//                case 0:
+//                    key.append((char) ((int) (rnd.nextInt(26)) + 97));
+//                    //  a~z  (ex. 1+97=98 => (char)98 = 'b')
+//                    break;
+//                case 1:
+//                    key.append((char) ((int) (rnd.nextInt(26)) + 65));
+//                    //  A~Z
+//                    break;
+//                case 2:
+//                    key.append((rnd.nextInt(10)));
+//                    // 0~9
+//                    break;
+//            }
+//        }
+//        return key.toString();
+//    }
+
     public static String createKey() {
-        StringBuffer key = new StringBuffer();
+        StringBuilder key = new StringBuilder();
         Random rnd = new Random();
 
-        for (int i = 0; i < 8; i++) { // 인증코드 8자리
-            int index = rnd.nextInt(3); // 0~2 까지 랜덤
-
-            switch (index) {
-                case 0:
-                    key.append((char) ((int) (rnd.nextInt(26)) + 97));
-                    //  a~z  (ex. 1+97=98 => (char)98 = 'b')
-                    break;
-                case 1:
-                    key.append((char) ((int) (rnd.nextInt(26)) + 65));
-                    //  A~Z
-                    break;
-                case 2:
-                    key.append((rnd.nextInt(10)));
-                    // 0~9
-                    break;
-            }
+        for (int i = 0; i < 6; i++) { // 인증코드 6자리
+            key.append(rnd.nextInt(10)); // 0~9 까지 랜덤
         }
+
         return key.toString();
     }
     @Override
